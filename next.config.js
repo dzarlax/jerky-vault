200a57d90d7fb15fbc2c174289dd0040d29881c9
Alexey Panfilov <me@dzarlax.dev>
2025-05-12 23:39:52 +0200

    feat(i18n): implement dynamic loading of next-translate-plugin with fallback configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-import nextTranslate from 'next-translate-plugin';
 import './src/env.js';
 
 /** @type {import('next').NextConfig} */
@@ -32,8 +31,27 @@ const nextConfig = {
   },
 };
 
+// Try to import next-translate-plugin, but don't fail if it's not available
+let finalConfig = nextConfig;
+try {
+  const nextTranslate = await import('next-translate-plugin').then(mod => mod.default || mod);
+  const i18nConfig = await import('./i18n.cjs').then(mod => mod.default || mod);
+  
+  finalConfig = nextTranslate({
+    ...nextConfig,
+    i18n: i18nConfig,
+  });
+} catch (error) {
+  console.warn('Warning: next-translate-plugin not available, using default config');
+  // If next-translate-plugin is not available, use the default config
+  finalConfig = {
+    ...nextConfig,
+    // Add basic i18n config if available
+    i18n: {
+      locales: ['en', 'ru', 'rs'],
+      defaultLocale: 'en',
+    },
+  };
+}
 
-export default nextTranslate({
-  ...nextConfig,
-  i18n: await import('./i18n.cjs').then(mod => mod.default || mod),
-});
+export default finalConfig;
